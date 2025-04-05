6d4095a73bb850f7a898708464cfbc9a59afc7f1
Husnain Taj <husnain.taj@gmail.com>
2025-04-05 14:38:01 +0500

    Enhance splash screen with animation and scale adjustments for logo

--- app/src/main/java/com/smd/l226786/sharedfast/SplashActivity.kt
+++ app/src/main/java/com/smd/l226786/sharedfast/SplashActivity.kt
@@ -2,18 +2,30 @@ package com.smd.l226786.sharedfast
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }, 3000) // 3 seconds delay
+        val splashLogo: ImageView = findViewById(R.id.splash_logo)
+        val jumpAnimation: Animation = AnimationUtils.loadAnimation(this, R.anim.jump)
+        splashLogo.startAnimation(jumpAnimation)
+
+        jumpAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation) {}
+
+            override fun onAnimationEnd(animation: Animation) {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {}
+        })
     }
 }
